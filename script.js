61ed14dc16364b660c4d24c4a4238806dc2b5b8a
Priya Shankar <preiyah@gmail.com>
2019-07-14 13:04:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.body.style.overflow = "hidden";
+  /*document.body.style.overflow = "hidden";
 
   document.getElementById("ques").style.display = "none";
   document.getElementById("nav-bar").style.display = "none";
   //document.getElementById('myNav').style.display='none';
   document.getElementById("navbar-small").style.display = "none";
-  document.getElementById("navbar-hr").style.display = "none";
+  document.getElementById("navbar-hr").style.display = "none";*/
 
   $("#toolset").click(function() {
     $("#base").animate({ marginLeft: "30" });
   });
 
-  document.getElementById("smiley").style.visibility = "hidden";
+ // document.getElementById("smiley").style.visibility = "hidden";
   document.getElementById("scroll").style.visibility = "hidden";
   document.getElementById("km").addEventListener("click", function() {
     $(this).fadeOut();
