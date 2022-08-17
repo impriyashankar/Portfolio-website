23538f3af97cc4857bc7f6439818516443ab6aac
priya shankar <preiyah@gmail.com>
2022-08-17 20:01:50 +0200

    implemented first scroll view

--- script.js
+++ script.js
@@ -1,43 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-  /*document.body.style.overflow = "hidden";
-
-  document.getElementById("ques").style.display = "none";
-  document.getElementById("nav-bar").style.display = "none";
-  //document.getElementById('myNav').style.display='none';
-  document.getElementById("navbar-small").style.display = "none";
-  document.getElementById("navbar-hr").style.display = "none";*/
-
-  $("#toolset").click(function() {
-    $("#base").animate({ marginLeft: "30" });
+$(document).ready(() => {
+  const $menuBars = $('.menu > i');
+  const $menuCross = $('.menu-icon-cross > i');
+  $('.menu-expanded').addClass('hide');
+  $menuBars.on('click', () => {
+    $('.menu-expanded').removeClass('menu-animated-hide');
+    $('.menu-expanded').removeClass('hide');
+    $('.menu-expanded').addClass('menu-animated-show');
+    // $('.menu-expanded').slideToggle();
   });
 
- // document.getElementById("smiley").style.visibility = "hidden";
-  document.getElementById("scroll").style.visibility = "hidden";
-  document.getElementById("km").addEventListener("click", function() {
-    $(this).fadeOut();
-    $("#ques").fadeIn(4000, function() {
-      $("#temp-blk").fadeOut(1000, function() {
-       // $("#smiley").css("visibility", "visible");
-        $("#scroll").css("visibility", "visible");
-        $("#nav-bar").fadeIn();
-        $("#navbar-small").fadeIn();
-        $("#navbar-hr").fadeIn();
-        document.body.style.overflow = "auto";
-      });
-    });
+  $menuCross.on('click', () => {
+    // $('.menu-expanded').hide();
+    $('.menu-expanded').removeClass('menu-animated-show');
+    $('.menu-expanded').addClass('menu-animated-hide');
+
+    $('.menu-expanded').addClass('hide');
+    // $('.menu-expanded').slideToggle();
+
+
   });
 });
-
-window.addEventListener("scroll", function() {
-  $("#smiley").css("visibility", "hidden");
-});
-
-function showDesc(option) {
-  //document.getElementById('toolxp').style.display = "none";
-  var slides = document.getElementsByClassName("tool-exp");
-
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[option - 1].style.display = "block";
-}
