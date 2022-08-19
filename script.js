3db730fc9c76158ca29c4284fb1d5ba792a096df
priya shankar <preiyah@gmail.com>
2022-08-20 00:21:03 +0200

    added project specific images

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
   const $menuBars = $('.menu > i');
   const $menuCross = $('.menu-icon-cross > i');
   $('.menu-expanded').addClass('hide');
+
   $menuBars.on('click', () => {
     $('.menu-expanded').removeClass('menu-animated-hide');
     $('.menu-expanded').removeClass('hide');
@@ -16,17 +17,38 @@ $(document).ready(() => {
 
     $('.menu-expanded').addClass('hide');
     // $('.menu-expanded').slideToggle();
-
-
   });
-  //Adding smooth scrolling to section links
 
-  $('a').on('click', (event) => {
-    console.log(this.hash);
-    event.preventDefault();
-    let hash = this.hash;
-    $('html, body').animate({ scrollTop: $(hash).offset.top }, 800, () => {
-      window.location.hash = hash;
-    });
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash;
+      });
+    }
   });
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 75) {
+      $('#navBar').css("opacity", 0.3);
+    }
+    else {
+      $('#navBar').css("opacity", 1);
+    }
+  });
+
+  $('#navBar').on("mouseover", function () {
+    $(this).css("opacity", 1);
+  });
+
+  $('#navBar').on("mouseleave", function () {
+    if ($(window).scrollTop() > 75) { // this here
+      $(this).css("opacity", 0.3);
+    }
+  });
+
+
 });
